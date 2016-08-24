0d99720b08419022ce828ee33339e3dd02cdacd2
Tracy Hinds <tracyhinds@gmail.com>
2016-08-23 22:49:41 -0400

    add styling and template for motivation

--- app/index.js
+++ app/index.js
@@ -1,12 +1,24 @@
 // teach our app how to GET
 var express = require('express');
 var app = express();
+var exphbs = require('express-handlebars');
+
+app.use(express.static('./app/public'));
+
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main',
+  layoutsDir: './app/views/layouts'
+}))
+
+app.set('views', './app/views');
+app.set('view engine', 'handlebars');
 
 var motivations = require('motivations');
 var pickOne = require('pick-one');
 
 app.get('/', function(request, response){
-  response.send(pickOne(motivations));
+  var motivation = pickOne(motivations);
+  response.render('motivation', { motivation});
 })
 
 module.exports = app;
