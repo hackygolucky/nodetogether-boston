72fea8f8d5a056a287dbdb7803e9e446e5bce6cc
Tracy Hinds <tracyhinds@gmail.com>
2016-08-23 22:13:30 -0400

    add encouragement to the webz

--- app/index.js
+++ app/index.js
@@ -2,8 +2,11 @@
 var express = require('express');
 var app = express();
 
+var motivations = require('motivations');
+var pickOne = require('pick-one');
+
 app.get('/', function(request, response){
-  response.send('oh hai!');
+  response.send(pickOne(motivations));
 })
 
 module.exports = app;
