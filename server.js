fc705848e9669ad25d3749a37d0db1ab7809e9bc
Tracy Hinds <tracyhinds@gmail.com>
2016-08-23 21:43:03 -0400

    remove your excess export silly bear

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
 
+var rules = require('./app');
+app.use(rules);
+
 var port = '8080';
 var host = '0.0.0.0';
 
-app.use(require('./app'));
 app.listen(port, host);
 
 console.log('Server running %s:%d...', host, port);
-
-module.exports = app;
